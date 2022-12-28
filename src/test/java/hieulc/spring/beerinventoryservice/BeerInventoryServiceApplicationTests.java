47d79d29677d69b0e91063379b9b7e4a1cb02165
hieulc <forjob.leconghieu@gmail.com>
2022-12-28 15:38:34 +0700

    Initial new Project

---
+++ src/test/java/hieulc/spring/beerinventoryservice/BeerInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hieulc.spring.beerinventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerInventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
